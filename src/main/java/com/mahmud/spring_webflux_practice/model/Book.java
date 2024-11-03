e413c6e1ccffbe9055a0d955e009fc93f817a7ee
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-11-03 22:22:24 +0600

    book model

---
+++ src/main/java/com/mahmud/spring_webflux_practice/model/Book.java
@@ -0,0 +1,21 @@
+package com.mahmud.spring_webflux_practice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String author;
+}
