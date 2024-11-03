64c33aa682367e2011d8cbf1b121f1a2c1cb93fb
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-11-03 22:22:32 +0600

    book service and repo

---
+++ src/main/java/com/mahmud/spring_webflux_practice/services/BookService.java
@@ -0,0 +1,24 @@
+package com.mahmud.spring_webflux_practice.services;
+
+import com.mahmud.spring_webflux_practice.model.Book;
+import com.mahmud.spring_webflux_practice.repositories.BookRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+}
